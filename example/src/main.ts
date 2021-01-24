3e2b4d4f1e534a4b093ba8812238445606e931a0
Jeffrey Dufour <74216106+jffr@users.noreply.github.com>
2021-01-24 15:52:38 +0100

    Initial setup

---
+++ example/src/main.ts
@@ -0,0 +1,21 @@
+import ScrollCanvas from 'scroll-canvas';
+
+const rootElement = document.querySelector('.root') as HTMLElement;
+const containerElement = document.querySelector('.example') as HTMLElement;
+const imagePaths = [...containerElement.getElementsByClassName('  example-image')].map(el => el.getAttribute('src'));
+
+if (containerElement !== null) {
+  const canvas = new ScrollCanvas({
+    className: 'example-canvas',
+    containerElement: containerElement,
+    rootElement: rootElement,
+    width: 800,
+    height: 800,
+    imagePaths: imagePaths as string[]
+  });
+
+  const wrapper = document.querySelector('.example-wrapper')!;
+  wrapper.innerHTML = '';
+  wrapper.append(canvas.canvas);
+  console.log(canvas);
+}
---
+++ lib/src/scroll-canvas.ts
@@ -0,0 +1,108 @@
+type TScrollCanvasOptions = {
+  width: number;
+  height: number;
+  imagePaths: string[];
+  containerElement: HTMLElement;
+  rootElement: HTMLElement;
+  className?: string;
+};
+
+// TODO: GIT
+// TODO: Create read me for instructions
+// TODO: Make constructor less big
+// TODO: Fix issues with rootElement
+// TODO: Add necessary CSS to the containerElement
+// TODO: Add error messages
+// TODO: Add IE11 support
+// TODO: Add demo images not from Maikel (or ask permission)
+// IDEA: Create React component?
+export default class ScrollCanvas {
+  private readonly _canvas: HTMLCanvasElement;
+  private readonly _containerElement: HTMLElement;
+  private readonly _rootElement: HTMLElement;
+  private _context: CanvasRenderingContext2D;
+  private _images: HTMLImageElement[];
+  private _currentIndex: number;
+  private _observer: IntersectionObserver;
+
+  constructor (options: TScrollCanvasOptions) {
+    const { width, height, imagePaths, className, rootElement, containerElement } = options;
+    this._canvas = document.createElement('canvas');
+    this._canvas.width = width;
+    this._canvas.height = height;
+    this._context = this._canvas.getContext('2d')!;
+    this._currentIndex = 0;
+    this._rootElement = rootElement;
+    this._containerElement = containerElement;
+    this._images = [];
+    this.handleScroll = this.handleScroll.bind(this);
+
+    if (className) {
+      this._canvas.classList.add(className);
+    }
+
+    this._observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0]?.isIntersecting) {
+          this._rootElement.addEventListener('scroll', this.handleScroll, { passive: true });
+        } else {
+          this._rootElement.removeEventListener('scroll', this.handleScroll);
+        }
+      },
+      { threshold: [0, 1] }
+    );
+
+    this.preloadImages(imagePaths).then((images) => {
+      if (images.length === 0) throw new Error('There are no images loaded for the canvas.');
+      this._images = images;
+      this._context.drawImage(this._images[0], 0, 0, this._canvas.width, this._canvas.height);
+      this._observer.observe(this._containerElement);
+    });
+  }
+
+  get canvas () {
+    return this._canvas;
+  }
+
+  destroy () {
+    this._rootElement.removeEventListener('scroll', this.handleScroll);
+    this._observer.unobserve(this._containerElement);
+    this._canvas.remove();
+  }
+
+  private createImage (imagePath: string): Promise<HTMLImageElement> {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = imagePath;
+      img.onload = () => resolve(img);
+      img.onerror = (error) => reject(error);
+      return img;
+    });
+  }
+
+  private updateImage (index: number) {
+    this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
+    this._context.drawImage(this._images[index], 0, 0, this._canvas.width, this._canvas.height);
+  }
+
+  private preloadImages (imagePaths: string[]) {
+    const images = [];
+    for (const imagePath of imagePaths) {
+      images.push(this.createImage(imagePath));
+    }
+    return Promise.all(images);
+  }
+
+  private handleScroll () {
+    requestAnimationFrame(() => {
+      const frameCount = this._images.length;
+      const maxScrollTop = this._containerElement.offsetHeight - window.innerHeight;
+      const scrollFraction = (this._rootElement.scrollTop - this._containerElement.offsetTop) / maxScrollTop;
+      const frameIndex = Math.max(0, Math.min(frameCount - 1, Math.ceil(scrollFraction * frameCount)));
+
+      if (frameIndex === this._currentIndex) return;
+      this._currentIndex = frameIndex;
+      this.updateImage(frameIndex);
+    });
+  }
+}
